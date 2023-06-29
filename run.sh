dca60281ad565cd294e233cc372e8e7c9cdb4d78
Derek <alexanderderekrein@gmail.com>
2023-06-29 21:01:57 +0200

    feat: define intervals

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-exec /bin/grafana-agent                                     \
-  --config.file=/etc/grafana-agent/agent.yaml                       \
-  --metrics.wal-directory=/etc/grafana-agent/data                   \
+exec /bin/grafana-agent \
+  --config.file=/etc/grafana-agent/agent.yaml \
+  --metrics.wal-directory=/etc/grafana-agent/data \
   --config.expand-env
