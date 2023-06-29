bc9929b9afb190f2020ce6ddb55772344f22ceae
Derek <alexanderderekrein@gmail.com>
2023-06-29 19:46:52 +0200

    Update run.sh
    Co-authored-by: Chris Smith <1979423+chris13524@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 exec /bin/grafana-agent                                     \
-  --config.file=/etc/agent/agent.yaml                       \
-  --metrics.wal-directory=/etc/agent/data                   \
+  --config.file=/etc/grafana-agent/agent.yaml                       \
+  --metrics.wal-directory=/etc/grafana-agent/data                   \
   --config.expand-env
